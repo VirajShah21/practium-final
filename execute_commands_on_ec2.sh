d272eeb60482cd77fc68d02c6989fa76160fb786
Viraj Shah <shahvirajd@gmail.com>
2022-04-26 04:57:35 -0400

    Fixed AWS issues

--- execute_commands_on_ec2.sh
+++ execute_commands_on_ec2.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-kill -9 $(lsof -t -i:80)
-echo "Killed process running on port 8080"
+kill -9 $(lsof -t -i:8080)
 
 ./jdk-14.0.2/bin/java -jar practium-final-0.0.1-SNAPSHOT.jar
-echo "Started server using java -jar command"
